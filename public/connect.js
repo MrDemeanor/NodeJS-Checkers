ca2d8cb0b21be93ef7b45def525635b42379a72a
btr26 <btr26@txstate.edu>
2018-09-21 21:47:49 -0500

    Can roughly calculate the centers of each checkers piece, no matter where on its square we click. This will allow us to draw a circle around a piece that we click showing that it has been selected. Still needs some adjusting, but the basic mechanics work

--- public/connect.js
+++ public/connect.js
@@ -14,11 +14,54 @@ if (window.innerHeight < window.innerWidth) {
 
 var c = canvas.getContext('2d')
 
+// Returns the mouse position
+function returnMousePos(canvas, evt) {
+    var rect = canvas.getBoundingClientRect();
+    return {
+        x: evt.clientX - rect.left,
+        y: evt.clientY - rect.top
+    };
+}
+
+function rgbToHex(r, g, b) {
+    if (r > 255 || g > 255 || b > 255)
+        throw "Invalid color component";
+    return ((r << 16) | (g << 8) | b).toString(16);
+}
+
+// Event listener that displays mouse coordinates
+canvas.addEventListener('mousedown', function (event) {
+    var mousePos = returnMousePos(canvas, event)
+
+    var pixelData = c.getImageData(mousePos.x, mousePos.y, 1, 1).data
+
+    var hex = "#" + ("000000" + rgbToHex(pixelData[0], pixelData[1], pixelData[2])).slice(-6);
+
+    console.log("X: " + mousePos.x + " Y:" + mousePos.y + " " + hex)
+    console.log("")
+    console.log("Circle Location")
+
+    var lengthOfSquare = Math.ceil(canvas.width / numRows)
+
+    var x = ((Math.ceil(mousePos.x / lengthOfSquare)) * lengthOfSquare) - (lengthOfSquare / 2) + 1.5
+    console.log("X: " + x)
+
+    var y = ((Math.ceil(mousePos.y / lengthOfSquare)) * lengthOfSquare) - (lengthOfSquare / 2) + 1.5
+    console.log("Y: " + y)
+
+    c.fillStyle = 'rgba(16,255,0,1)'
+
+    c.beginPath()
+    c.arc(x, y , 5, Math.PI * 2, false)
+    c.fill();
+    c.stroke()
+})
+
 // Variables to modify canvas properties
 var isWhite = false;
 var numRows = 9
-var player1Color = 'pink'
-var player2Color = 'yellow'
+var player1Color = 'red'
+var player2Color = 'blue '
 
 /* 
     Function that draws the initial gameboard, and deterines if a square on the board should have
@@ -52,7 +95,7 @@ for (var i = 0, y = 0; i < numRows; i++ , y += canvas.width / numRows) {
     if (isWhite) {
         if (i >= 0 && i < 3) {
             draw(0, 0, y, false, true, player1Color)
-        } else if(i < numRows && i >= numRows - 3) {
+        } else if (i < numRows && i >= numRows - 3) {
             draw(0, 0, y, false, true, player2Color)
         } else {
             draw(0, 0, y, false, false)
@@ -61,7 +104,7 @@ for (var i = 0, y = 0; i < numRows; i++ , y += canvas.width / numRows) {
     } else {
         if ((i >= 0 && i < 3)) {
             draw(0, 0, y, true, true, player1Color)
-        } else if(i < numRows && i >= numRows - 3) {
+        } else if (i < numRows && i >= numRows - 3) {
             draw(0, 0, y, true, true, player2Color)
         } else {
             draw(0, 0, y, true, false)
@@ -69,3 +112,4 @@ for (var i = 0, y = 0; i < numRows; i++ , y += canvas.width / numRows) {
         isWhite = !isWhite
     }
 }
+
